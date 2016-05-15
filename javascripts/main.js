731e1e8a9ec79d737a9849735bffd20676df3a71
Garrett MacKay <garrett.mackay@gmail.com>
2016-05-15 18:22:46 -0400

    Created ChaMap javascript object and geolocation
    will want to wrap all of the functionality that we want to be embeddable
    in other projects in this file.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,95 @@
-console.log('This would be the main JS file.');
+var ChaMap = function (map) {
+    this.map = map;
+
+}
+ChaMap.prototype.WatchMapPosition = function () {
+    if (navigator.geolocation)
+    {
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge:10
+        }
+        this.currentPosition = navigator.geolocation.getCurrentPosition(function (location) {
+            this.map.addSource("location", {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [location.coords.longitude, location.coords.latitude]
+                        },
+                        "properties": {
+                            "title": "current-location",
+                            "marker-symbol": "star"
+                        }
+                    }]
+                }
+            });
+
+            this.map.addLayer({
+                "id": "location",
+                "type": "symbol",
+                "source": "location",
+                "layout": {
+                    "icon-image": "{marker-symbol}-15",
+                    "icon-size": 1,
+                    "text-field": "",
+                    "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                    "text-offset": [0, 0.6],
+                    "text-anchor": "top"
+                }
+            });
+        });
+
+        
+        this.watchId = navigator.geolocation.watchPosition(success,error);
+        
+    }
+    function success(pos) {
+        this.map.removeLayer("location");
+        this.map.removeSource("location");
+
+        this.map.addSource("location", {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [pos.coords.longitude, pos.coords.latitude]
+                    },
+                    "properties": {
+                        "title": "current-location",
+                        "marker-symbol": "star"
+                    }
+                }]
+            }
+        });
+
+        this.map.addLayer({
+            "id": "location",
+            "type": "symbol",
+            "source": "location",
+            "layout": {
+                "icon-image": "{marker-symbol}-15",
+                "icon-size": 1,
+                "text-field": "",
+                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                "text-offset": [0, 0.6],
+                "text-anchor": "top"
+            }
+        });
+  
+        
+    }
+    function error(error) {
+        console.log(error);
+    }
+}
+
+
+
